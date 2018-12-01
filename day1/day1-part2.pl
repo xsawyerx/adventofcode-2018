dca117128da24337b4aa68168a6d18397a6591a2
Sawyer X <xsawyerx@cpan.org>
2018-12-01 19:29:44 +0200

    Providing cleaner, alternative solution

--- day1/day1-part2.pl
+++ day1/day1-part2.pl
@@ -3,6 +3,7 @@ use strict;
 use warnings;
 use Path::Tiny qw< path >;
 
+## Solution 1: Using a function
 sub calibrate {
     my ( $lines, $num, $seen ) = @_;
 
@@ -15,12 +16,39 @@ sub calibrate {
     return;
 }
 
-my @lines = path('input')->lines( { 'chomp' => 1 } );
-my %seen  = ( 0 => 1 );
-my ( $num, $result );
+sub solution_1 {
+    my @lines = path('input')->lines( { 'chomp' => 1 } );
+    my %seen  = ( 0 => 1 );
+    my ( $num, $result );
 
-while ( !defined $result ) {
-    $result = calibrate( \@lines, \$num, \%seen );
+    while ( !defined $result ) {
+        $result = calibrate( \@lines, \$num, \%seen );
+    }
+
+    print "$result\n";
 }
 
-print "$result\n";
+## Solution 2: Using a callback function
+sub solution_2 {
+    my @lines = path('input')->lines( { 'chomp' => 1 } );
+    my %seen  = ( 0 => 1 );
+    my ( $num, $result );
+
+    my $calibrate = sub {
+        foreach my $change (@lines) {
+            $num += $change;
+            $seen{$num}++
+                and return $num;
+        }
+
+        return;
+    };
+
+    while ( !defined $result ) {
+        $result = $calibrate->();
+    }
+
+    print "$result\n";
+}
+
+solution_2();
